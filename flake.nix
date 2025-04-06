f43450d840ed0765b7fa77ba4344eb7242bcc422
Thomas Boerger <thomas@webhippie.de>
2025-04-06 13:11:40 +0200

    chore: define proper flake config including ignores

--- flake.nix
+++ flake.nix
@@ -6,25 +6,77 @@
       url = "github:nixos/nixpkgs/nixpkgs-unstable";
     };
 
-    utils = {
-      url = "github:numtide/flake-utils";
+    devenv = {
+      url = "github:cachix/devenv";
+    };
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+    };
+
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
     };
   };
 
-  outputs = { self, nixpkgs, utils }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          config = { allowUnfree = true; };
-        };
-      in
-      {
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            nodejs_20
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devenv.flakeModule
+        inputs.git-hooks.flakeModule
+      ];
+
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          imports = [
+            {
+              _module.args.pkgs = import inputs.nixpkgs {
+                inherit system;
+                config.allowUnfree = true;
+              };
+            }
           ];
+
+          devenv = {
+            shells = {
+              default = {
+                git-hooks = {
+                  hooks = {
+                    nixfmt-rfc-style = {
+                      enable = true;
+                    };
+                  };
+                };
+
+                languages = {
+                  javascript = {
+                    enable = true;
+                    package = pkgs.nodejs_22;
+                  };
+                };
+
+                packages = with pkgs; [
+                  nixfmt-rfc-style
+                ];
+              };
+            };
+          };
         };
-      }
-    );
+    };
 }
